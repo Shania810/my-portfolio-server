23fe90bb274ba182fa989797e318b283efe16e8d
Shania <shaniaantezana@gmail.com>
2023-08-15 20:05:39 -0300

    Created project routes

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ const express = require("express")
 const app = express()
 const connectDB = require("./db/db.config")
 const skillRoutes = require("./routes/skill.routes")
+const projectRoutes = require("./routes/project.routes")
 
 connectDB()
 app.use("/",skillRoutes)
+app.use("/",projectRoutes)
 
 module.exports = app
---
+++ routes/project.routes.js
@@ -0,0 +1,34 @@
+const { Router } = require("express")
+const Project = require("../models/project.model")
+const router = Router()
+
+router.get("/project", async (req, res) => {
+    try {
+        const projects = await Project.find()
+        res.status(200).json(projects)
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+    }
+})
+
+router.post("/project", async (req, res) => {
+    const newProject = req.body
+    try {
+        const project = await Project.create(newProject)
+        res.status(201).json(project)
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+    }
+})
+
+router.delete("/project/:id", async (req, res) => {
+    const { id } = req.params
+    try {
+        await Project.findByIdAndDelete(id)
+        res.status(200).json()
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+    }
+})
+
+module.exports = router
