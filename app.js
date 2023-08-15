98bbddfc58329279db492a91d858895537a0385a
Shania <shaniaantezana@gmail.com>
2023-08-15 20:20:03 -0300

    Created experience routes

--- app.js
+++ app.js
@@ -4,10 +4,12 @@ const connectDB = require("./db/db.config")
 const skillRoutes = require("./routes/skill.routes")
 const projectRoutes = require("./routes/project.routes")
 const feedbackRoutes = require("./routes/feedback.routes")
+const experienceRoutes = require("./routes/experience.routes")
 
 connectDB()
-app.use("/",skillRoutes)
-app.use("/",projectRoutes)
-app.use("/",feedbackRoutes)
+app.use("/", skillRoutes)
+app.use("/", projectRoutes)
+app.use("/", feedbackRoutes)
+app.use("/", experienceRoutes)
 
 module.exports = app
---
+++ routes/experience.routes.js
@@ -0,0 +1,34 @@
+const { Router } = require("express")
+const Experience = require("../models/experience.model")
+const router = Router()
+
+router.get("/experience", async (req, res) => {
+    try {
+        const experiences = await Experience.find()
+        res.status(200).json(experiences)
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+    }
+})
+
+router.post("/experience", async (req, res) => {
+    const newExperience = req.body
+    try {
+        const experience = await Experience.create(newExperience)
+        res.status(201).json(experience)
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+    }
+})
+
+router.delete("/experience/:id", async (req, res) => {
+    const { id } = req.params
+    try {
+        await Experience.findByIdAndDelete(id)
+        res.status(200).json()
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+    }
+})
+
+module.exports = router
