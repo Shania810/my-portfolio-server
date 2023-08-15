0873d1efca48da56fff97c508320c76d1c3850d5
Shania <shaniaantezana@gmail.com>
2023-08-15 20:30:16 -0300

    Inserted dependencies in app.js

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 const express = require("express")
-const app = express()
+const cors = require("cors")
+const morgan = require("morgan")
 const connectDB = require("./db/db.config")
 const skillRoutes = require("./routes/skill.routes")
 const projectRoutes = require("./routes/project.routes")
 const feedbackRoutes = require("./routes/feedback.routes")
 const experienceRoutes = require("./routes/experience.routes")
 
+const app = express()
 connectDB()
+
+app.use(morgan("dev"))
+app.use(cors())
+app.use(express.json())
+
 app.use("/", skillRoutes)
 app.use("/", projectRoutes)
 app.use("/", feedbackRoutes)
