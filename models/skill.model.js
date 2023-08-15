12e2aae5eacfea79fe91e13cf8839b84fe18025a
Shania <shaniaantezana@gmail.com>
2023-08-15 17:17:52 -0300

    Created model Skill

---
+++ models/skill.model.js
@@ -0,0 +1,8 @@
+const { Schema, model } = require("mongoose")
+
+const skillSchema = new Schema({
+    title: { type: String, required: [true, 'title is required'], lowercase: true, trim: true, unique: true },
+    image: { type: String, required: [true, 'image is required'], lowercase: true, trim: true, unique: true }
+})
+
+module.exports = model("Skill", skillSchema)
