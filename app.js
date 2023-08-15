899ca0685a5d4ee9757ae1782598cb10f2e7ba28
Shania <shaniaantezana@gmail.com>
2023-08-15 20:12:09 -0300

    Created feedback routes

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ const app = express()
 const connectDB = require("./db/db.config")
 const skillRoutes = require("./routes/skill.routes")
 const projectRoutes = require("./routes/project.routes")
+const feedbackRoutes = require("./routes/feedback.routes")
 
 connectDB()
 app.use("/",skillRoutes)
 app.use("/",projectRoutes)
+app.use("/",feedbackRoutes)
 
 module.exports = app
---
+++ routes/feedback.routes.js
@@ -0,0 +1,34 @@
+const { Router } = require("express")
+const Feedback = require("../models/feedback.model")
+const router = Router()
+
+router.get("/feedback", async (req, res) => {
+    try {
+        const feedbacks = await Feedback.find()
+        res.status(200).json(feedbacks)
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+    }
+})
+
+router.post("/feedback", async (req, res) => {
+    const newFeedback = req.body
+    try {
+        const feedback = await feedback.create(newFeedback)
+        res.status(201).json(feedback)
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+    }
+})
+
+router.delete("/feedback/:id", async (req, res) => {
+    const { id } = req.params
+    try {
+        await Feedback.findByIdAndDelete(id)
+        res.status(200).json()
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+    }
+})
+
+module.exports = router
