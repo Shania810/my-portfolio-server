6100c7952114c43d69a11a5197a74ea89869baea
Shania <shaniaantezana@gmail.com>
2023-08-19 13:37:42 -0300

    Changed model feedback

--- models/feedback.model.js
+++ models/feedback.model.js
@@ -1,7 +1,7 @@
 const { Schema, model } = require("mongoose")
 
 const feedbackSchema = new Schema({
-    person: { type: String, required: [true, "person is required"], lowercase: true, trim: true, unique: true },
+    person: { type: Schema.Types.ObjectId, ref: "Person", required: true },
     description: { type: String, required: [true, "description is required"], lowercase: true, trim: true, unique: true }
 })
 
